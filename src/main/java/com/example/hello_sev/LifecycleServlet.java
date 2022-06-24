c5fb82623a492d1d1d882fd531b4be06e7155146
주동호 <100278301+JOODON@users.noreply.github.com>
2022-06-25 00:20:01 +0900

    서블릿 배워보자!

---
+++ src/main/java/com/example/hello_sev/LifecycleServlet.java
@@ -0,0 +1,26 @@
+package com.example.hello_sev;
+
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+@WebServlet("/LifecycleServlet")
+public class LifecycleServlet extends HttpServlet {
+    private static final long serialVersionUID = 1L;
+
+    public LifecycleServlet(){
+        System.out.println("LifecycleServlet 생성!");
+    }
+    public void init(ServletConfig config)throws ServletException{
+        System.out.println("init 호출!");
+    }
+    public void destroy(){
+        System.out.println("destroy 호출!");
+    }
+    protected void service(HttpServletRequest request,HttpServletResponse response) throws ServletException, IOException{
+        System.out.println("service 호출!");
+    }
+}
